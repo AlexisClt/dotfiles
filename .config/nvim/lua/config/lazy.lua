f93e99c51d776d36f8d29d1bca914cf7232b1801
AlexisClt <61407749+AlexisClt@users.noreply.github.com>
2024-11-27 21:03:20 +1100

    Chenged colorscheme tree structure

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		-- import your plugins
+		{ import = "plugins.colorschemes.tokyonight" },
 		{ import = "plugins" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
