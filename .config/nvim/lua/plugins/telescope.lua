54214756aa891a41b4282ad4ff66218b1469a59e
Alexis Cullet <61407749+AlexisClt@users.noreply.github.com>
2025-02-17 21:22:51 +0100

    progress on support for images in terminal

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
     { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    { "nvim-telescope/telescope-media-files.nvim" },
   },
   config = function()
     -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -66,6 +67,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- Enable Telescope extensions if they are installed
     pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "media_files")
     pcall(require("telescope").load_extension, "ui-select")
 
     -- See `:help telescope.builtin`
