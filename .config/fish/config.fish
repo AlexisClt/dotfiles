7bc517523ef564f888aa114aad914016ad0a46b0
Alexis Cullet <61407749+AlexisClt@users.noreply.github.com>
2025-06-29 10:23:01 +0200

    Add SSH to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     set -Ux DBUS_SESSION_BUS_ADDRESS '$DBUS_LAUNCHD_SESSION_BUS_SOCKET'
 end
 set -x GPG_TTY (tty)
+
+ssh-add -q --apple-use-keychain ~/.ssh/id_ed25519 > /dev/null 2>&1
+
